3cd9b21cb24f9a7401b564fced8571905d84224d
bhavin-thumar <bhavin_thumar_83@live.com>
2020-08-09 15:37:14 +0530

    C progamming added

---
+++ C_Programming/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b;
+	printf("Enter value of a\n");
+	scanf("%d", &a);
+
+
+	printf("Enter value of b\n");
+	scanf("%d", &b);
+
+	printf("The sum is %d\n", a+b);
+
+
+	return 0;
+}
