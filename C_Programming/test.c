71b8152bee34c4ca9d5a8cadcc6e35c7684b697b
bhavin-thumar <bhavin_thumar_83@live.com>
2020-08-09 16:10:07 +0530

    Operator explained

--- C_Programming/test.c
+++ C_Programming/test.c
@@ -3,13 +3,18 @@
 int main()
 {
 	int a;
+	int c;
 	float b;
 	a = 50; 
 	b = 8;
+	c = 10;
 
-	printf("The value of a * b : %f\n", a*b);
+	printf("The value of a * b : %f\n", a*b); // %f if resultant is type float
 	printf("The value of a / b : %f\n", a/b);
 	printf("The value of a + b : %f\n", a+b);
+	printf("The value of a + c : %d\n", a+c); // %d if resultant is type int
+	printf("The value of a / c : %d\n", a/c);
+	
 
 	return 0;
 }
